3cded2147adac65644f03bc039d17977d0c7ad1d
Alessio Ottaviani <alessiottaviani@protonmail.com>
2024-06-24 15:23:26 +0200

    first

---
+++ src/main/java/com/alessiottavo/reactive_programming_with_reactor_3/ReactiveProgrammingWithReactor3Application.java
@@ -0,0 +1,13 @@
+package com.alessiottavo.reactive_programming_with_reactor_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveProgrammingWithReactor3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveProgrammingWithReactor3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/alessiottavo/reactive_programming_with_reactor_3/ReactiveProgrammingWithReactor3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alessiottavo.reactive_programming_with_reactor_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveProgrammingWithReactor3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
