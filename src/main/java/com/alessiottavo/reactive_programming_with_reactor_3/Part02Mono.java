302a74f92cba6acf7ae33ac09612fb58d8a7d46f
Alessio Ottaviani <alessiottaviani@protonmail.com>
2024-06-24 19:46:15 +0200

    Added Flux tests, simple Mono class

---
+++ src/main/java/com/alessiottavo/reactive_programming_with_reactor_3/Part02Mono.java
@@ -0,0 +1,42 @@
+package com.alessiottavo.reactive_programming_with_reactor_3;
+
+import reactor.core.publisher.Mono;
+
+/**
+* Learn how to create Mono instances.
+*
+* @author Sebastien Deleuze
+* @see <a href="https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html">Mono Javadoc</a>
+*/
+public class Part02Mono {
+
+//========================================================================================
+
+	// TODO Return an empty Mono
+	Mono<String> emptyMono() {
+		return null;
+	}
+
+//========================================================================================
+
+	// TODO Return a Mono that never emits any signal
+	Mono<String> monoWithNoSignal() {
+		return null;
+	}
+
+//========================================================================================
+
+	// TODO Return a Mono that contains a "foo" value
+	Mono<String> fooMono() {
+		return null;
+	}
+
+//========================================================================================
+
+	// TODO Create a Mono that emits an IllegalStateException
+	Mono<String> errorMono() {
+		return null;
+	}
+
+}
+
---
+++ src/test/java/com/alessiottavo/reactive_programming_with_reactor_3/Part01FluxTests.java
@@ -0,0 +1,53 @@
+package com.alessiottavo.reactive_programming_with_reactor_3;
+
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+import reactor.test.StepVerifier;
+
+public class Part01FluxTests {
+	
+	Part01Flux testee = new Part01Flux();
+	
+	@Test
+	void expectEmptyFlux() {
+		
+		StepVerifier.create(testee.emptyFlux())
+		.verifyComplete();
+		
+	}
+	
+	@Test
+	void expectFooBarFromValues() {
+		
+		StepVerifier.create(testee.fooBarFluxFromValues())
+		.expectNext("foo", "bar")
+		.verifyComplete();
+	}
+	
+	@Test
+	void expectFooBarFromList() {
+		
+		StepVerifier.create(testee.fooBarFluxFromValues())
+		.expectNextSequence(Arrays.asList("foo", "bar"))
+		.verifyComplete();
+	}
+	
+	@Test
+	void expectError() {
+		
+		StepVerifier.create(testee.errorFlux())
+		.expectError()
+		.verify();
+	}
+	
+	@Test
+	void expectCounter() {
+		
+		StepVerifier.create(testee.counter())
+		.expectNext(0L, 1L, 2L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
+		.verifyComplete();
+	}
+
+}
